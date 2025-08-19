5810dd1f11ae5ef3a7912b5bf09e0ca58a5f034a
kavinrajmurugan <kavinraj@andiran.in>
2025-08-19 21:29:52 +0530

    .

--- lib/core/api/deal_api.dart
+++ lib/core/api/deal_api.dart
@@ -2,12 +2,12 @@ import 'package:roobai/core/api/app_api.dart';
 import 'package:roobai/core/theme/constants.dart';
 
 class DealApi {
-  static const String base_api_url = "base_api_url"; 
+  static const String base_api_url = "base_api_url";
   // static const String defaultCategory = 'deal/';
   static const int defaultPageNumber = 1;
 
   /// Build a clean URL with no double slashes
-   static var dealurl='${APIS.baseurl}/$base_api_url/$just_in/$defaultPageNumber'; 
+  static var dealurl = '${APIS.baseurl + base_api_url}/$just_in/$defaultPageNumber';
 
   static final header = APIS.header;
   static final headers = APIS.headers;
